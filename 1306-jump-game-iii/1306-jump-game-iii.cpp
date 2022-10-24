cc617c068cce0aafe7d6ee041b29ada5cd35f797
TrishaSrivastava002 <77577646+TrishaSrivastava002@users.noreply.github.com>
2022-10-24 19:16:57 +0530

    Time: 98 ms (51.34%), Space: 35.8 MB (31.72%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 bool solve(vector<int>& arr,vector<int> &dp, int s){
   if(s<0 || s>=arr.size()) return 0;
   if(arr[s]==0) return 1;
-  if(dp[s]!=-1) return 0;
+  if(dp[s]!=-1) return dp[s];
     dp[s]=0;
   return dp[s]=max(solve(arr,dp,s+arr[s]),solve(arr,dp,s-arr[s]));  
 }
