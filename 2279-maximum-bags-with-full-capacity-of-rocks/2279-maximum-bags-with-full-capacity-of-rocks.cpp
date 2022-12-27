6999b0be3a5933ef33c89383038b363c699e94f0
TrishaSrivastava002 <77577646+TrishaSrivastava002@users.noreply.github.com>
2022-12-27 11:18:21 +0530

    Time: 202 ms (89.01%), Space: 89.7 MB (12.75%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -3,14 +3,23 @@ public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
         vector<int> v;
         int k=0,sum=0,n=rocks.size();
-       for(int i=0;i<n;i++){
-       rocks[i]=capacity[i]-rocks[i];
-    }
-    sort(rocks.begin(),rocks.end());
-    for(int i=0;i<n;i++){
-        sum+=rocks[i];
-        if(sum>additionalRocks) break;
-        else k++;
+    //    for(int i=0;i<n;i++){
+    //    rocks[i]=capacity[i]-rocks[i];
+    // }
+    // sort(rocks.begin(),rocks.end());
+    // for(int i=0;i<n;i++){
+    //     sum+=rocks[i];
+    //     if(sum>additionalRocks) break;
+    //     else k++;
+    //     }
+        for(int i=0;i<n;i++) v.push_back(capacity[i]-rocks[i]);
+        sort(v.begin(),v.end());
+        for(int i=0;i<n;i++){
+            if(additionalRocks>=v[i]){
+              additionalRocks-=v[i];
+                k++;
+            } 
+            else break;
         }
     return k;
     }
