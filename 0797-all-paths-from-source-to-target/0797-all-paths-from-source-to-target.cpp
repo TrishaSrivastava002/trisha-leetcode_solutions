be71a3b174aa139f262b6956fb2aa54a3c573277
TrishaSrivastava002 <77577646+TrishaSrivastava002@users.noreply.github.com>
2022-12-30 11:41:50 +0530

    Time: 31 ms (40.69%), Space: 18.4 MB (13.64%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,25 +1,30 @@
 class Solution {
-    public:
-    vector<vector<int>> v1;
-    void dfs(vector<vector<int>>& graph,int i,vector<int> &v){
-        int n=graph.size(),m=graph[0].size();
-     v.push_back(i);
-        for(auto j:graph[i]){
-            if(j==n-1){
-                v.push_back(j);
-                v1.push_back(v);
-                v.pop_back();   
-                    }
-               else{
-                dfs(graph,j,v);
-                v.pop_back();    
-               }    
-            }           
-        }
-    
-vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph){   
-     vector<int> v;    
-   dfs(graph,0,v);
-     return v1;
+ void dfs(vector<vector<int>>& graph,int i,vector<int> temp,vector<int> vis,vector<vector<int>> &ans,vector<int> adj[],int des){
+//      we are not passing temp & vis by address as we do not want to push all thr values in continuity of the other paths so no need to clear temp & vis
+     if(i==des){
+         temp.push_back(i);
+         ans.push_back(temp);
+         return ;
+     }
+     vis[i]=1;
+     temp.push_back(i);
+     for(auto it:adj[i]){
+         if(!vis[it]) dfs(graph,it,temp,vis,ans,adj,des);
+     }
+ }
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    int n=graph.size();
+    vector<int> adj[n]; 
+    for(int i=0;i<n;i++){
+    for(auto it:graph[i]){
+        adj[i].push_back(it);
+    }
+    }
+    vector<int> temp,vis(n,0);
+    vector<vector<int>> ans;
+     //we only need to traverse the paths which starts from 0
+     dfs(graph,0,temp,vis,ans,adj,n-1);       
+      return ans;
     }
 };
