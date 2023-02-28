66639d3d5ed38b2c5efa5a2595fc33aa66ebe476
TrishaSrivastava002 <77577646+TrishaSrivastava002@users.noreply.github.com>
2023-02-28 23:08:40 +0530

    Time: 42 ms (50.76%), Space: 30 MB (20.97%) - LeetHub

---
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+private:
+    TreeNode* solve(istringstream &ss){
+        string s;
+        ss >> s;
+        if(s=="null") return NULL;
+      TreeNode* root = new TreeNode(stoi(s));
+      root->left=solve(ss);
+      root->right=solve(ss);
+        return root;
+    }
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root){
+     return !root ? " null" : " " + to_string(root->val) + serialize(root->left) + serialize(root->right);    
+        
+    }
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data){
+        istringstream ss(data);
+        return solve(ss);
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
