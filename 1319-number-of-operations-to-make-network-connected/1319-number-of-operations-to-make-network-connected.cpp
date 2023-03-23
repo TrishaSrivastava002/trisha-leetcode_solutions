881d9a64b51c804a0a4437253c43a4706269668d
TrishaSrivastava002 <77577646+TrishaSrivastava002@users.noreply.github.com>
2023-03-23 09:03:22 +0530

    Time: 105 ms (96.24%), Space: 38 MB (87.74%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,22 +1,26 @@
-
-class Solution {
-public:
-    vector<int> parent;
-    int findparent(int node){
-        if(node == parent[node])
+class Solution
+{
+    public:
+        vector<int> parent;
+    int findparent(int node)
+    {
+        if (node == parent[node])
             return node;
         return parent[node] = findparent(parent[node]);
     }
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1) return -1;
-        int k=n;
-        for (int i=0;i<n;i++) parent.push_back(i);
-        for(int i=0;i<connections.size();i++){
-          if(findparent(connections[i][0])!=findparent(connections[i][1])){
-             parent[findparent(connections[i][0])]=findparent(connections[i][1]);
-               k--;
-          }    
-        }                                                      
- return k-1;// -1 because a node in loop is it's own parent
+    int makeConnected(int n, vector<vector < int>> &connections)
+    {
+        if (connections.size() < n - 1) return -1;
+        int k = n;
+        for (int i = 0; i < n; i++) parent.push_back(i);
+        for (int i = 0; i < connections.size(); i++)
+        {
+            if (findparent(connections[i][0]) != findparent(connections[i][1]))
+            {
+                parent[findparent(connections[i][0])] = findparent(connections[i][1]);
+                k--;	//looking for nodes which are it's own parent
+            }
+        }
+        return k - 1;	// -1 because a node in loop is it's own parent
     }
 };
