2e4981f8bc47932418385f5a2e4156aadeca9e1e
Trisha Srivastava <77577646+TrishaSrivastava002@users.noreply.github.com>
2023-04-07 10:04:25 +0530

    Time: 79 ms (60.00%), Space: 33 MB (33.12%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -2,7 +2,6 @@ class Solution {
 void solve(vector<vector<int>>& grid,int i,int j){
     int m=grid.size(),n=grid[0].size();
     if(i>m-1 || j>n-1 || i<0 || j<0 || grid[i][j]==0) return ;
-    
     else{
         grid[i][j]=0;
    solve(grid,i+1,j);
@@ -16,9 +15,9 @@ public:
         int m=grid.size(),n=grid[0].size(),res=0;
        vector<vector<int>> dp(m,vector<int>(n,-1));
         
-        for(int i=0;i<m;i++){
+          for(int i=0;i<m;i++){
             if(i==0 || i==m-1){
-             for(int j=0;j<n;j++){
+          for(int j=0;j<n;j++){
                  if(grid[i][j]==1) solve(grid,i,j); 
              }
             }
@@ -26,13 +25,12 @@ public:
                if(grid[i][0]==1)  solve(grid,i,0);
                 if(grid[i][n-1]==1) solve(grid,i,n-1);
             }
-        }
+            }
         for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
            if(grid[i][j]==1) res++; 
         }    
         }
-         
         return res;
     }
 };
